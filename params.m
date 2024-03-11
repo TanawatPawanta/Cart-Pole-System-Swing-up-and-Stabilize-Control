17062f0de8b39a8c05e3896f070c7433cf037e34
Fzil0n <tanawat84598@gmail.com>
2024-03-11 21:46:07 +0700

    collected pole's data 15/45/90

--- params.m
+++ params.m
@@ -1,4 +1,21 @@
-m = 0.065;
-l = 0.57;
-b = 0.001;
-g = 9.81;
+m = 0.093;  %Kg
+l = 0.52;   %m
+b = 0.01;
+innertia = 0.00209; %Kg*m^2
+g = 9.81;   %m/s^2
+
+% Pole_pos15 = out.polePosition.data;
+% Pole_velo15 = out.poleVelocity.data;
+% Pole_velo15 = Pole_velo15(:); 
+% 
+% start = 1662;
+% stop = 2401;
+% Pole_pos15_use = Pole_pos15(start:stop);
+% Pole_velo15_use = Pole_velo15(start:stop);
+% time = (0:0.01:(stop-start)*0.01)';
+% save("PoleData15")
+%% 
+% plot phase space
+plot(Pole_pos90_use,Pole_velo90_use);
+xlabel("theta")
+ylabel("thetadot")
